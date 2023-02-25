411724f2774d444cb37451597d93e7470118722d
Seamus Lowry <spl1owry3@gmail.com>
2023-02-25 13:41:38 -0500

    basic sign in off tutorial

--- app/src/main/java/seamuslowry/hundredandten/MainActivity.kt
+++ app/src/main/java/seamuslowry/hundredandten/MainActivity.kt
@@ -1,6 +1,8 @@
 package seamuslowry.hundredandten
 
+import android.content.IntentSender
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,11 +12,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import seamuslowry.hundredandten.ui.theme.HundredAndTenTheme
 
+private const val TAG = "MainActivity"
+
 class MainActivity : ComponentActivity() {
+
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             HundredAndTenTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,6 +37,49 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        oneTapClient = Identity.getSignInClient(this)
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(
+                BeginSignInRequest.PasswordRequestOptions.builder()
+                    .setSupported(true)
+                    .build(),
+            )
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build(),
+            )
+            // Automatically sign in when exactly one credential is retrieved.
+            .setAutoSelectEnabled(true)
+            .build()
+
+        // TODO update to account for situations where this shouldn't happen all the time
+        oneTapClient.beginSignIn(signInRequest)
+            .addOnSuccessListener(this) { result ->
+                try {
+                    startIntentSenderForResult(
+                        result.pendingIntent.intentSender,
+                        1,
+                        null,
+                        0,
+                        0,
+                        0,
+                        null,
+                    )
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            .addOnFailureListener(this) { e ->
+                // No saved credentials found. Launch the One Tap sign-up flow, or
+                // do nothing and continue presenting the signed-out UI.
+                e.localizedMessage?.let { Log.d(TAG, it) }
+            }
     }
 }
 
