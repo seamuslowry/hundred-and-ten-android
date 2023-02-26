584ce6c391fe70d5f965084e4f8a51bdeb278bec
Seamus Lowry <spl1owry3@gmail.com>
2023-02-26 13:07:01 -0500

    use more succinct sign in

--- app/src/main/java/seamuslowry/hundredandten/MainActivity.kt
+++ app/src/main/java/seamuslowry/hundredandten/MainActivity.kt
@@ -1,11 +1,11 @@
 package seamuslowry.hundredandten
 
-import android.content.Intent
-import android.content.IntentSender
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,19 +15,39 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.CommonStatusCodes
 import seamuslowry.hundredandten.ui.theme.HundredAndTenTheme
 
 private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
 
-    private val REQ_ONE_TAP = 666
+    private val tokenRequestOptions by lazy {
+        BeginSignInRequest.GoogleIdTokenRequestOptions.Builder()
+            .setSupported(true)
+            .setServerClientId(getString(R.string.client_id))
+            .setFilterByAuthorizedAccounts(true)
+            .build()
+    }
 
-    private lateinit var oneTapClient: SignInClient
-    private lateinit var signInRequest: BeginSignInRequest
+    private val activityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartIntentSenderForResult(),
+    ) { result ->
+        when (result.resultCode) {
+            RESULT_OK -> {
+                try {
+                    val signInCredentials = Identity.getSignInClient(this)
+                        .getSignInCredentialFromIntent(result.data)
+
+                    Log.d(TAG, "Will eventually use: ${signInCredentials.googleIdToken}")
+                } catch (e: ApiException) {
+                    Log.e(TAG, "Sign-in failed with error code:", e)
+                }
+            }
+            else -> Log.e(TAG, "Sign-in failed")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -43,96 +63,18 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        oneTapClient = Identity.getSignInClient(this)
-        signInRequest = BeginSignInRequest.builder()
-            .setPasswordRequestOptions(
-                BeginSignInRequest.PasswordRequestOptions.builder()
-                    .setSupported(false) // TODO check this out; see how to disallow getting passwords back
+        Identity.getSignInClient(this)
+            .beginSignIn(
+                BeginSignInRequest.Builder()
+                    .setGoogleIdTokenRequestOptions(tokenRequestOptions)
                     .build(),
             )
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-                    .setServerClientId(getString(R.string.client_id))
-                    // Only show accounts previously used to sign in.
-                    .setFilterByAuthorizedAccounts(true)
-                    .build(),
-            )
-            // Automatically sign in when exactly one credential is retrieved.
-            .setAutoSelectEnabled(true)
-            .build()
-
-        // TODO update to account for situations where this shouldn't happen all the time
-        oneTapClient.beginSignIn(signInRequest)
-            .addOnSuccessListener(this) { result ->
-                try {
-                    // possible ref for deprecation warning: https://developers.google.com/identity/one-tap/android/linked-account-signin-client
-                    startIntentSenderForResult(
-                        result.pendingIntent.intentSender,
-                        REQ_ONE_TAP,
-                        null,
-                        0,
-                        0,
-                        0,
-                        null,
-                    )
-                } catch (e: IntentSender.SendIntentException) {
-                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
-                }
+            .addOnSuccessListener { result ->
+                activityResultLauncher.launch(IntentSenderRequest.Builder(result.pendingIntent.intentSender).build())
             }
-            .addOnFailureListener(this) { e ->
-                // No saved credentials found. Launch the One Tap sign-up flow, or
-                // do nothing and continue presenting the signed-out UI.
-                e.localizedMessage?.let { Log.d(TAG, it) }
+            .addOnFailureListener { e ->
+                Log.e(TAG, "Sign-in failed because:", e)
             }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        when (requestCode) {
-            REQ_ONE_TAP -> {
-                try {
-                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
-                    val idToken = credential.googleIdToken
-                    val password = credential.password
-                    when {
-                        idToken != null -> {
-                            // Got an ID token from Google. Use it to authenticate
-                            // with your backend.
-                            Log.d(TAG, "Got ID token.")
-                        }
-                        password != null -> {
-                            // Got a saved username and password. Use them to authenticate
-                            // with your backend.
-                            Log.d(TAG, "Got password.")
-                        }
-                        else -> {
-                            // Shouldn't happen.
-                            Log.d(TAG, "No ID token or password!")
-                        }
-                    }
-                } catch (e: ApiException) {
-                    when (e.statusCode) {
-                        CommonStatusCodes.CANCELED -> {
-                            Log.d(TAG, "One-tap dialog was closed.")
-                        }
-                        CommonStatusCodes.NETWORK_ERROR -> {
-                            Log.d(TAG, "One-tap encountered a network error.")
-                            // Try again or just ignore.
-                        }
-                        else -> {
-                            Log.d(
-                                TAG,
-                                "Couldn't get credential from result." +
-                                    " (${e.localizedMessage})",
-                            )
-                        }
-                    }
-                }
-            }
-        }
     }
 }
 
