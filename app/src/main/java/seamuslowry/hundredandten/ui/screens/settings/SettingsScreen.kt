ebff3aa2c3684615a9a25b6c4c3253a24c00b3a3
Seamus Lowry <spl1owry3@gmail.com>
2023-03-14 17:06:43 -0400

    better formatting

--- app/src/main/java/seamuslowry/hundredandten/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/seamuslowry/hundredandten/ui/screens/settings/SettingsScreen.kt
@@ -3,8 +3,11 @@ package seamuslowry.hundredandten.ui.screens.settings
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -12,9 +15,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.launch
 import seamuslowry.hundredandten.R
+import seamuslowry.hundredandten.ui.theme.HundredAndTenTheme
 
 @Composable
 fun SettingsScreen(
@@ -32,25 +38,50 @@ fun SettingsScreen(
     }
 
     Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter,
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
+        LogoutButton(
+            onClick = { scope.launch { viewModel.signOut() } },
+            enabled = state !is SettingsState.Loading && state !is SettingsState.LoggedOut,
+            error = state is SettingsState.Error,
+        )
+    }
+}
+
+@Composable
+fun LogoutButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    error: Boolean = false,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(
+            onClick = onClick,
+            enabled = enabled,
+            modifier = Modifier.fillMaxWidth(),
         ) {
-            Button(
-                onClick = { scope.launch { viewModel.signOut() } },
-                enabled = state !is SettingsState.Loading && state !is SettingsState.LoggedOut,
-            ) {
-                if (state is SettingsState.Loading) {
-                    CircularProgressIndicator()
-                }
-                Text(text = stringResource(R.string.log_out))
+            if (!enabled) {
+                CircularProgressIndicator()
             }
-            when (state) {
-                is SettingsState.Error -> Text(text = stringResource(R.string.log_out_error))
-                else -> {}
-            }
+            Text(text = stringResource(R.string.log_out))
         }
+        if (error) {
+            Text(text = stringResource(R.string.log_out_error), color = MaterialTheme.colorScheme.error)
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun LogoutPreview() {
+    HundredAndTenTheme {
+        LogoutButton(onClick = { /*TODO*/ }, error = true)
     }
 }
