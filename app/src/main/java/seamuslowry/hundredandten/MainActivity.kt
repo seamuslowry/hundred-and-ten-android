d9de56c14766bb6e660aa4a83e2b281b6ec80c89
Seamus Lowry <spl1owry3@gmail.com>
2023-03-14 21:08:31 -0400

    go fullscreen; should probably hide navigaton in fullscreen

--- app/src/main/java/seamuslowry/hundredandten/MainActivity.kt
+++ app/src/main/java/seamuslowry/hundredandten/MainActivity.kt
@@ -1,20 +1,26 @@
 package seamuslowry.hundredandten
 
 import android.os.Bundle
+import android.view.WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import seamuslowry.hundredandten.ui.theme.HundredAndTenTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.attributes.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+
         setContent {
             HundredAndTenTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("Android")
+                    Greeting("Android", modifier = Modifier.systemBarsPadding())
                 }
             }
         }
@@ -30,14 +36,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HundredAndTenTheme {
-        Greeting("Android")
-    }
+fun Greeting(name: String, modifier: Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
 }
